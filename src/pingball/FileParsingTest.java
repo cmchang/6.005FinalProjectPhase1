644ecd3b406139dd78936e7b4e0cc271dae2ea7e
Carolyn Chang <changc@mit.edu>
2014-04-16 22:54:15 -0400

    Added file parser testing strategy

---
+++ src/pingball/FileParsingTest.java
@@ -0,0 +1,31 @@
+package pingball;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testing reading in a file and outputting a board
+ * 
+ * Testing Strategy:
+ * (A) Varying formatted files
+ *      (A1) Contains a comment 
+ *      (A2) Comment in file contains a keyword (i.e. 'ball', 'bumper', etc)
+ *      (A2) Doesn't contain a comment
+ *      (A3) Additional spaces added randomly
+ *      (A4) An object in the field doesn't contain all the correct fields
+ *      (A5) Objects declared in the file over lap each other
+ * (B) Objects in the board
+ *      (B1) Empty board
+ *      (B2) Contains one ball
+ *      (B3) Contains multiple balls
+ *      (B4) Contains no ball but contains at least one gadget
+ *      (B5) Contains at least one gadget
+ *      (B6) Contains every possible gadget
+ *      
+ *
+ */
+
+public class FileParsingTest {
+
+    
+    
+}
