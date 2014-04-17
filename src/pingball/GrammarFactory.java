d02ffcd4cc3cffd31d3315b8684a5f1f901c5ab6
Carolyn Chang <changc@mit.edu>
2014-04-17 14:55:38 -0400

    Added new constructor GrammarFactor

--- src/pingball/GrammarFactory.java
+++ src/pingball/GrammarFactory.java
@@ -50,6 +50,38 @@ public class GrammarFactory {
         return BoardCreatorListener.getBoard();
     }
     
+    public static Board parse(String fileStr) {
+        String input = fileStr;
+        // Create a stream of tokens using the lexer.
+        CharStream stream = new ANTLRInputStream(input);
+        GrammarLexer lexer = new GrammarLexer(stream);
+        lexer.reportErrorsAsExceptions();
+        TokenStream tokens = new CommonTokenStream(lexer);
+        
+        // Feed the tokens into the parser.
+        GrammarParser parser = new GrammarParser(tokens);
+        parser.reportErrorsAsExceptions();
+        
+        // Generate the parse tree using the starter rule.
+        ParseTree tree = parser.boardInfo(); // "expression" is the starter rule
+        
+        // debugging option #1: print the tree to the console
+//        System.err.println(tree.toStringTree(parser));
+
+        // debugging option #2: show the tree in a window
+//        ((RuleContext)tree).inspect(parser);
+
+        // debugging option #3: walk the tree with a listener
+//        new ParseTreeWalker().walk(new PrintEverythingListener(), tree);
+        
+        // Finally, construct an Expression value by walking over the parse tree.
+        ParseTreeWalker walker = new ParseTreeWalker();
+        BoardCreatorListener listener = new BoardCreatorListener();
+        walker.walk(listener, tree);
+
+        return BoardCreatorListener.getBoard();
+    }
+    
     public static String FileToString(File file){
         String textString = "";
 
